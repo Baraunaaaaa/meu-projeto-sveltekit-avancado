8cc12a3c459ffb03d85247ab4d49b67aa74d09d6
baraunaaaaa <manoelabarauna5@gmail.com>
2025-06-06 10:07:59 -0400

    é isso ai ee

---
+++ src/routes/03/dummy/users/+page.server.js
@@ -0,0 +1,6 @@
+export async function load({fetch}) {
+    const res = await fetch ('https://dummyjson.com/users');
+    const users = await res.json ();
+    return {users}; 
+    
+}
---
+++ src/routes/03/dummy/users/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let {data} =$props();
+</script>
+
+<h1>Usuários</h1>
+<ul>
+    {#each data.users.users as user}
+    <li>
+        <a href= "03/dummy/users/{user.id}">
+            {user.firstName} {user.lastname} ({user.age} anos)
+        </a>
+    </li>
+  {/each}
+</ul>
