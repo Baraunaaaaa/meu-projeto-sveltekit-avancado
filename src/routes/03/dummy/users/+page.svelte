1218f389f113bc7c9cf97983dc3a55ba774f2076
baraunaaaaa <manoelabarauna5@gmail.com>
2025-06-10 11:25:42 -0400

    vamobora

--- src/routes/03/dummy/users/+page.svelte
+++ src/routes/03/dummy/users/+page.svelte
@@ -3,6 +3,10 @@
 </script>
 
 <h1>Usuários</h1>
+<form>
+    <input name="idade" type="number" placeholder="Digite uma idade" />
+    <button>Filtrar</button>
+</form>
 <ul>
     {#each data.users.users as user}
     <li>
