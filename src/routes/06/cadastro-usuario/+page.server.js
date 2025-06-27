22a192720a1ec394dd0fc87c0f5f5879a99ecf18
baraunaaaaa <manoelabarauna5@gmail.com>
2025-06-27 08:52:43 -0400

    olaaa

--- src/routes/06/cadastro-usuario/+page.server.js
+++ src/routes/06/cadastro-usuario/+page.server.js
@@ -1,38 +1,51 @@
-import {fail, redirect} from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit' ;
 
-function contem(texto,caracteres) {
+
+
+
+function contem(texto, caracteres) {
     for (const caractere of caracteres)
-        if (texto.includes(caractere)) return true;
-    return false;
+        if (texto.includes(caractere)) return true ;
+    return false ;
 }
 
-export const actions ={
-    default: async ({request}) => {
-        const data = await request.FormData ();
+
+
+
+export const actions = {
+    default: async ({ request }) => {
+        const data = await request.formData();
         const dados = {
-            nome: data.get ('nome'), email: data.get ('email'),
-            nascimento: data.get ('nascimento'), senha: data.get ('senhaa'),
+            nome: data.get ('nome'), email: data. get ('email'),
+            nascimento: data.get ('nascimento'), senha: data.get ('senha'),
             confirmacaosenha: data.get ('confirmacaosenha'), erros: []
         }
-        if (!dados.nome || !dados.email || !dados.nascimento || !dados.senha || !dados.confirmacaosenha) dados.erros.push('preencha todos os campos.');
+        if (!dados.nome || !dados.email || !dados.nascimento || !dados.senha || !dados.confirmacaosenha) dados.erros.push ('preencha todos os campos.') ;
+        if (!dados.email.includes ('@'))  dados.erros.push('email inválido.') ;
+        if (dados.senha != dados.confirmacaosenha) dados.erros.push ('senhas não conferem.') ;
+        if (!contem(dados.senha,"abcdefghijklmnopqrstuvwxyz")
+            || !contem (dados.senha, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+            || !contem (dados.senha, "0123456789")
+            || !contem (dados.senha, "!@#$%¨&*()-_=+")
+        )
+            dados.erros.push ('A senha deve ter pelo menos uma letra maiuscula, uma minuscula, um numero e um caracte especial') ;
+            let agora = new Date (), nasc = new Date (dados.nascimento) ;
+            if (agora - nasc < 378691200000)
+                dados.erros.push ('Você ainda não completou 12 anos!') ;
 
-        if (!dados.email.includes('@')) dados.erros.push ('Email inválido.');
 
-        if (dados.senha != dados.confirmacaosenha) dados.erros.push('Senhas nao conferem.');
 
-        if (!contem(dados.senha, "abcdefghijklmnopqrstuvwxyz")
-            || !contem(dados.senha, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-            || !contem(dados.senhas, "0123456789")
-            || !contem(dados.senhas, "!@#$%&*()-_=+")
-)
-       dados.erros.push ('A senha deve tetr pelo menos uma letra maiúscula, uma minúscula, um numero e um caractere especial');
 
-       let agora= new Date(), nasc= new Date (dados.nascimento);
-       if (agora - nasc < 378691200000)
-           dados.erros.push('Voce ainda não completou 12 anos!');
+            if (dados.erros.length > 0 ) return fail (400, dados) ;
 
-       if (dados.erros.length > 0) return fail (400, dados);
 
-       redirect (303, '06/profile');
+
+
+            redirect (303, '/06/profile');
     }
 };
+
+
+
+
+
--- src/routes/06/cadastro-usuario/+page.svelte
+++ src/routes/06/cadastro-usuario/+page.svelte
@@ -1,19 +1,25 @@
-<script>
-    let {form}= $props ();
+<script>    
+    export let form;
 </script>
 
-<h2> Cadastro de usuário</h2>
+<h2>Cadastro de usuário</h2>
+
 <form method="POST">
-    <imput name="nome" type="text" placeholder="Nome de usuário" value={form?.nome || ''} required >
-    <imput name="email" type="email" placeholder="E-mail de usuário" value={form?.email || ''} required>
-    <imput name="senha" type="passoword" placeholder="Senha" value={form?.senha || ''} required>
-    <imput name="confimarcaosenha" type="passoword" placeholder="Confirmarção de senha" value={form?.confirmacaosenha || ''} required>
-    <imput name="nascimento" type="date"  value={form?.nascimento || ''} required>
+    <input name="nome" type="text" placeholder="Nome de usuário" value={form?.nome || ''} required />
+    
+    <input name="email" type="email" placeholder="E-mail de usuário" value={form?.email || ''} required />
+    
+    <input name="senha" type="password" placeholder="Senha" value={form?.senha || ''} required />
+    
+    <input name="confirmacaosenha" type="password" placeholder="Confirmação de senha" value={form?.confirmacaosenha || ''} required />
+    
+    <input name="nascimento" type="date" value={form?.nascimento || ''} required />
+    
     <button>Cadastrar</button>
 </form>
 
-{#if form?.erros.length >0}
-{#each form.erros as erros}
-<p style= "color:red">{erro}</p>
-{/each}
+{#if form?.erros?.length > 0}
+    {#each form.erros as erro}
+        <p style="color: red">{erro}</p>
+    {/each}
 {/if}
